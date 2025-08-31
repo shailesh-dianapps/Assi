90cc3de6c592db4e16c1c294630cf066f0b7e055
shailesh-dianapps <shailesh.mangal@dianapps.com>
2025-08-31 19:56:22 +0530

    Commit

--- src/models/studentModel.js
+++
@@ -1,8 +0,0 @@
-const connectDB = require("../connection/conn");
-
-async function getStudentCollection(){
-    const db = await connectDB();
-    return db.collection("students");
-}
-
-module.exports = getStudentCollection;
